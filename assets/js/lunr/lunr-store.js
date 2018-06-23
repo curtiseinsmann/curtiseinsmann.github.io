4c364e3c646aa4c882efb7a840ed400b54006e40
Curtis Einsmann <curteins@vt.edu>
2018-06-23 13:44:35 -0400

    Changed links with build command

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "Defensive Ruby Coding for Amazon S3's TestEvent",
         "excerpt":"Whenenever you come across the s3:TestEvent notification from Amazon Simple Storage Service (S3), it may come as an unpleasant surprise. It depends on how you’ve written your code to parse your Amazon Simple Notification Service (SNS) notifications. Fortunately, we can do a deep dive and illustrate how we can code...","categories": ["technical"],
         "tags": ["ruby","aws","s3","sns","sqs"],
-        "url": "http://localhost:4000/technical/defensive-ruby-coding-for-amazon-s3-testevent/",
+        "url": "https://curtiseinsmann.github.io/technical/defensive-ruby-coding-for-amazon-s3-testevent/",
         "teaser":null}]
