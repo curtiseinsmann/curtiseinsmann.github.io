dd9105c8f126ecd2d129283c8a9091df1be51351
Curtis Einsmann <curteins@vt.edu>
2018-06-24 21:33:28 -0400

    Merged the edits in

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Defensive Ruby Coding for Amazon S3's TestEvent",
-        "excerpt":"Whenenever you come across the s3:TestEvent notification from Amazon Simple Storage Service (S3), it may come as an unpleasant surprise. It depends on how you’ve written your code to parse your Amazon Simple Notification Service (SNS) notifications. Fortunately, we can do a deep dive and illustrate how we can code...","categories": ["technical"],
+        "excerpt":"The s3:TestEvent notification from Amazon S3 may come as an unpleasant surprise. It depends on how you’ve written code to parse your Amazon SNS notifications. In this post, we’ll do a deep dive into how we can code defensively against these kinds of anomalies in Ruby. Initial Architecture Let’s consider...","categories": ["technical"],
         "tags": ["ruby","aws","s3","sns","sqs"],
         "url": "https://curtiseinsmann.github.io/technical/defensive-ruby-coding-for-amazon-s3-testevent/",
         "teaser":null}]
