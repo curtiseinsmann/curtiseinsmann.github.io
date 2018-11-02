ba388ea5f1212dd7439f78964ef768263084c6f0
Curtis Einsmann <curteins@vt.edu>
2018-11-02 17:29:18 -0400

    Made some updates for AWS Educate

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Defensive Ruby Against Amazon S3's TestEvent",
-        "excerpt":"The s3:TestEvent notification from Amazon S3 may come as an unpleasant surprise. It depends on how you’ve written code to parse your Amazon SNS notifications. In this post, we’ll do a deep dive into how we can code defensively against these kinds of anomalies in Ruby. Initial Architecture Let’s consider...","categories": ["technical"],
+        "excerpt":"The s3:TestEvent notifications from Amazon S3 can be pretty useful to ensure that your notifications are correctly configured. However, there are certain instances in which a lack of defensive coding can get you into trouble. It depends on how you’ve written code to parse your Amazon SNS notifications. In this...","categories": ["technical"],
         "tags": ["ruby","aws","s3","sns","sqs"],
         "url": "https://curtiseinsmann.com/technical/defensive-ruby-against-amazon-s3-testevent/",
         "teaser":null}]
