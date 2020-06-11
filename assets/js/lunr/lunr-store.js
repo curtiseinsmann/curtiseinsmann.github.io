fba7659fa57eafdc4f290dd85aef2e3145f8c531
Curtis Einsmann <curteins@vt.edu>
2020-06-10 21:56:14 -0400

    BLM donations

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"The s3:TestEvent notifications from Amazon S3 can be pretty useful to ensure that your notifications are correctly configured. However, there are certain instances in which a lack of defensive coding can get you into trouble. It depends on how you’ve written code to parse your Amazon SNS notifications. In this...","categories": ["technical"],
         "tags": ["ruby","aws","s3","sns","sqs"],
         "url": "https://curtiseinsmann.com/technical/defensive-ruby-against-amazon-s3-testevent/",
+        "teaser":null},{
+        "title": "Black Lives Matter",
+        "excerpt":"My employer, Amazon, will 100% match employee contributions to the following organizations, up to $10,000 per employee. I am accepting donations through my personal PayPal or Venmo and will message you the receipt after I submit your donation through an Amazon internal website. Please submit donations by 11:59pm EST on...","categories": ["donations"],
+        "tags": ["blacklivesmatter"],
+        "url": "https://curtiseinsmann.com/donations/black-lives-matter/",
         "teaser":null}]
