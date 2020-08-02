4c2d83ff5fa2eec349449fcff161816342e0bd37
Curtis Einsmann <curteins@vt.edu>
2020-08-02 11:28:35 -0400

    Update home page for Twitter, archive BLM

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,1 @@
-var store = [{
-        "title": "Black Lives Matter",
-        "excerpt":"My employer, Amazon, will 100% match employee contributions to the following organizations, up to $10,000 per employee. I am accepting donations through my personal Venmo or PayPal in an attempt to maximize this opportunity. I will message you the receipt after I submit your donation through an Amazon internal website....","categories": ["donations"],
-        "tags": ["blacklivesmatter"],
-        "url": "https://curtiseinsmann.com/donations/black-lives-matter/",
-        "teaser":null}]
+var store = []
