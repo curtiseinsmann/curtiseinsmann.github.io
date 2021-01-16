abeabc69c8dec336708a10cf619cb05e734daf66
Curtis Einsmann <curteins@vt.edu>
2021-01-16 16:32:05 -0500

    Jan 2021 update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,11 @@
-var store = []
+var store = [{
+        "title": "Amazon shouldn't have hired me. Here's how I became an SDE2.",
+        "excerpt":"This article was published on Medium on August 10, 2020. Medium link, no paywall. ","categories": ["stories"],
+        "tags": [],
+        "url": "https://curtiseinsmann.com/stories/amazon-shouldnt-have-hired-me-heres-how-i-became-an-sde2/",
+        "teaser":null},{
+        "title": "I've code reviewed over 750 pull requests at Amazon. Here's my exact thought process.",
+        "excerpt":"This article was published on Medium on November 10, 2020. Medium link, no paywall. ","categories": ["stories"],
+        "tags": [],
+        "url": "https://curtiseinsmann.com/stories/ive-code-reviewed-over-750-pull-requests-at-amazon-heres-my-exact-thought-process/",
+        "teaser":null}]
